dc52f4f3cf19b57924520e42f289916865cfc203
taidev198 <traigiuxom9x@gmail.com>
2018-11-11 14:30:20 +0700

    demo 2:add some tables in admin layout.

---
+++ src/controller/AdminController.java
@@ -0,0 +1,72 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminController implements Initializable {
+    @FXML
+    AnchorPane anchorPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    private void user(){
+        Parent user = null;
+        try {
+            user = FXMLLoader.load(getClass().getResource("/view/User.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        anchorPane.getChildren().clear();
+        anchorPane.getChildren().addAll(user);
+    }
+
+    @FXML
+    private void grammar(){
+        Parent user = null;
+        try {
+            user = FXMLLoader.load(getClass().getResource("/view/AdminGrammar.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        anchorPane.getChildren().clear();
+        anchorPane.getChildren().addAll(user);
+    }
+
+    @FXML
+    private void testGrammar(){
+        Parent user = null;
+        try {
+            user = FXMLLoader.load(getClass().getResource("/view/AdminTestGrammar.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        anchorPane.getChildren().clear();
+        anchorPane.getChildren().addAll(user);
+    }
+
+    @FXML
+    private void quizTest(){
+        Parent user = null;
+        try {
+            user = FXMLLoader.load(getClass().getResource("/view/AdminQuizzTest.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        anchorPane.getChildren().clear();
+        anchorPane.getChildren().addAll(user);
+    }
+}
