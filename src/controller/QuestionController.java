5129cedbeae42363e38d2a6e5892e25235ba0422
taidev198 <traigiuxom9x@gmail.com>
2018-11-12 15:22:05 +0700

    demo 2:completely grammar scene.

--- src/controller/QuestionController.java
+++ src/controller/QuestionController.java
@@ -3,177 +3,84 @@ package controller;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.WeakChangeListener;
 import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.util.Callback;
+import model.TestGrammarModel;
 
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class QuestionController implements Initializable {
 
     @FXML
     ListView<Label> grammarLists;
     @FXML
-    ListView<Question> listView;
+    ListView<TestGrammarModel.Question> listView;
     @FXML
     AnchorPane anchorPane;
+
+    @FXML
+    Text description;
+
+    private TestGrammarModel model;
+    private Map<String, List<List<String>>> listQuestion;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        model = new TestGrammarModel();
+        listQuestion = model.getTestGrammar();
+        List<Map.Entry<String, List<List<String>>>> entryList = new ArrayList<>(listQuestion.entrySet());
+        List<String> key = new ArrayList<>();
+        for (Map.Entry<String, List<List<String>>> entry :
+               entryList ) {
+            Label label = new Label(entry.getKey() );
+            key.add(entry.getKey());
+            label.setGraphic( new ImageView( new Image("/resource/avatar.png")));
+            grammarLists.getItems().add(label);
+        }
 
         for (int i = 0; i < 14; i++) {
             Label label = new Label("AS, When Or While ,AS, When Or While" );
             label.setGraphic( new ImageView( new Image("/resource/avatar.png")));
             grammarLists.getItems().add(label);
         }
-
-
-        Question q =  new Question("hello", "You can easily increase the size of the Radio Button and Checkbox icons in SurveyGizmo surveys so that they are easier for your survey respondents to select.", "a","b",
-                "c", "d", "", "a", 1);
-        Question q1 =  new Question("hello", "hello", "b","b",
-                "c", "d", "", "a", 1);
-        Question q2 =  new Question("hello", "hello", "c","b",
-                "c", "d", "", "a", 1);
-        Question q3 =  new Question("hello", "hello", "d","b",
-                "c", "d", "", "a", 1);
-        Question q4 =  new Question("hello", "hello", "e","b",
-                "c", "d", "", "a", 1);
-        Question q5 =  new Question("hello", "hello", "f","b",
-                "c", "d", "", "a", 1);
-        Question q6 =  new Question("hello", "hello", "g","b",
-                "c", "d", "", "a", 1);
-         listView.getItems().addAll(q);
-
-        listView.getItems().addAll(q1);
-        listView.getItems().addAll(q2);
-        listView.getItems().addAll(q3);
-        listView.getItems().addAll(q4);
-        listView.getItems().addAll(q5);
-        listView.getItems().addAll(q6);
-        listView.setCellFactory(new Callback<ListView<Question>, ListCell<Question>>() {
+        grammarLists.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            int index = grammarLists.getSelectionModel().getSelectedIndex();
+            List<List<String>> tmp = entryList.get(index).getValue();
+            description.setText(key.get(index));
+            for (int i =0; i< tmp.get(1).size(); i++ ) {
+                listView.getItems().addAll( new TestGrammarModel.Question(key.get(index), tmp.get(1).get(i), tmp.get(2).get(i),tmp.get(3).get(i),
+                        tmp.get(4).get(i), tmp.get(5).get(i), "", tmp.get(6).get(i), Integer.valueOf(tmp.get(0).get(i))));
+            }
+        });
+        listView.setCellFactory(new Callback<ListView<TestGrammarModel.Question>, ListCell<TestGrammarModel.Question>>() {
             @Override
-            public ListCell<Question> call(ListView<Question> param) {
+            public ListCell<TestGrammarModel.Question> call(ListView<TestGrammarModel.Question> param) {
                 return new QuestionCell();
             }
         });
-         listView.getItems().addListener(new ListChangeListener<Question>() {
+         listView.getItems().addListener(new ListChangeListener<TestGrammarModel.Question>() {
              @Override
-             public void onChanged(Change<? extends Question> c) {
+             public void onChanged(Change<? extends TestGrammarModel.Question> c) {
                  while (c.next()){
-                     System.out.println(c.getFrom());
+                 //    System.out.println(c.getFrom());
                  }
              }
          });
         listView.setFocusTraversable( false );
-
-
     }
 
-   private static class Question{
-        private String content;
-        private String question;
-        private String optionA;
-        private String optionB;
-        private String optionC;
-        private String optionD;
-        private String ans;
-        private String key;
-        private int number;
 
-        public Question(String content, String question, String optionA, String optionB,
-                        String optionC, String optionD, String ans, String key, int number) {
-            this.content = content;
-            this.question = question;
-            this.optionA = optionA;
-            this.optionB = optionB;
-            this.optionC = optionC;
-            this.optionD = optionD;
-            this.ans = ans;
-            this.key = key;
-            this.number = number;
-        }
-
-       public int getNumber() {
-           return number;
-       }
-
-       public void setNumber(int number) {
-           this.number = number;
-       }
-
-       public String getContent() {
-            return content;
-        }
-
-        public void setContent(String content) {
-            this.content = content;
-        }
-
-        public String getQuestion() {
-            return question;
-        }
-
-        public void setQuestion(String question) {
-            this.question = question;
-        }
-
-        public String getOptionA() {
-            return optionA;
-        }
-
-        public void setOptionA(String optionA) {
-            this.optionA = optionA;
-        }
-
-        public String getOptionB() {
-            return optionB;
-        }
-
-        public void setOptionB(String optionB) {
-            this.optionB = optionB;
-        }
-
-        public String getOptionC() {
-            return optionC;
-        }
-
-        public void setOptionC(String optionC) {
-            this.optionC = optionC;
-        }
-
-        public String getOptionD() {
-            return optionD;
-        }
-
-        public void setOptionD(String optionD) {
-            this.optionD = optionD;
-        }
-
-        public String getAns() {
-            return ans;
-        }
-
-        public void setAns(String ans) {
-            this.ans = ans;
-        }
-
-        public String getKey() {
-            return key;
-        }
-
-        public void setKey(String key) {
-            this.key = key;
-        }
-    }
-
-    private class QuestionCell extends ListCell<Question>{
+    private class QuestionCell extends ListCell<TestGrammarModel.Question>{
         private HBox content;
         private HBox choose;
         private Text question;
@@ -186,7 +93,7 @@ public class QuestionController implements Initializable {
         private Text number;
         private VBox vBox;
         ChangeListener<Boolean> radioListener = (src, ov, nv) -> radioChanged(nv);
-        WeakChangeListener<Boolean> weakRadioListener = new WeakChangeListener(radioListener);
+        WeakChangeListener weakRadioListener = new WeakChangeListener(radioListener);
 
         public QuestionCell(){
             super();
@@ -223,11 +130,14 @@ public class QuestionController implements Initializable {
         }
 
         @Override
-        protected void updateItem(Question item, boolean empty) {
+        protected void updateItem(TestGrammarModel.Question item, boolean empty) {
             super.updateItem(item, empty);
             if (item !=null && !empty){
                 question.setText(item.getQuestion());
                 optionA.setText(item.getOptionA());
+                optionB.setText(item.getOptionB());
+                optionC.setText(item.getOptionC());
+                optionD.setText(item.getOptionD());
                 number.setText(String.valueOf(item.getNumber()) + ": ");
                 setGraphic(vBox);
                 if (optionA.isSelected()){
@@ -240,14 +150,9 @@ public class QuestionController implements Initializable {
         protected void radioChanged(boolean selected) {
             if (selected && getListView() != null && !isEmpty() && getIndex() >= 0) {
                 getListView().getSelectionModel().select(getIndex());
-                System.out.println(getListView().getSelectionModel().getSelectedItem().getOptionA());
-                if (optionA.isSelected()){
-                    System.out.println("thanh tai nguyen");
-                }else if (optionB.isSelected())
-                    System.out.println(optionA.getText());
+                System.out.println(getListView().getSelectionModel().getSelectedItem().getKey());
 
             }
         }
-
     }
 }
