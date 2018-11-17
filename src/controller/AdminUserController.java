5c24701a45b3a1ad7c91ed3dea76a451bba9a121
taidev198 <traigiuxom9x@gmail.com>
2018-11-17 08:17:16 +0700

    demo 3:completing AdminUserView Scene.

--- src/controller/AdminUserController.java
+++ src/controller/AdminUserController.java
@@ -1,26 +1,195 @@
 package controller;
 
 import helper.LoadSceneAble;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import model.TestGrammarModel;
+import model.UserModel;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
  * Created by traig on 9:59 AM, 11/15/2018
  */
 public class AdminUserController implements Initializable, LoadSceneAble {
+    @FXML
+     TableView<UserModel.User> tableView;
+     UserModel model;
+    ObservableList<UserModel.User> data;
+    @FXML
+    Text filterText;
+    List<MenuButton> menuButton;
+    FilteredList<UserModel.User> filteredList;
+    private  List<List<String>> listUser;
+    @FXML
+    TableColumn<TestGrammarModel.QuestionTableView, String> idCol;
+    @FXML
+    TableColumn<TestGrammarModel.QuestionTableView, String> usernameCol;
+    @FXML
+    TableColumn<TestGrammarModel.QuestionTableView, String> passwordCol;
+    @FXML
+    TableColumn<TestGrammarModel.QuestionTableView, String> firstnameCol;
+    @FXML
+    TableColumn<TestGrammarModel.QuestionTableView, String> lastnameCol;
+    @FXML
+    TableColumn<TestGrammarModel.QuestionTableView, String> addressCol;
+    @FXML
+    TableColumn<TestGrammarModel.QuestionTableView, String> phoneCol;
+    @FXML
+    TableColumn<TestGrammarModel.QuestionTableView, String> emailCol;
+    @FXML
+    TableColumn<TestGrammarModel.QuestionTableView, String> birthCol;
+    @FXML
+    TableColumn<TestGrammarModel.QuestionTableView, MenuButton> actionCol;
+
+    //textFields
+    @FXML
+    TextField idText;
+    @FXML
+    TextField usernameText;
+    @FXML
+    TextField passwordText;
+    @FXML
+    TextField firstnameText;
+    @FXML
+    TextField lastnameText;
+    @FXML
+    TextField addressText;
+    @FXML
+    TextField phoneText;
+    @FXML
+    TextField emailText;
+    @FXML
+    TextField birthText;
+    @FXML
+    TextField searchingField;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        model = new UserModel();
+        listUser = model.getUserInfo();
+        idCol.setCellValueFactory(new PropertyValueFactory<>("userInfoid"));
+        usernameCol.setCellValueFactory(new PropertyValueFactory<>("username"));
+        passwordCol.setCellValueFactory(new PropertyValueFactory<>("password"));
+        firstnameCol.setCellValueFactory(new PropertyValueFactory<>("address"));
+        lastnameCol.setCellValueFactory(new PropertyValueFactory<>("email"));
+        addressCol.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        phoneCol.setCellValueFactory(new PropertyValueFactory<>("firstname"));
+        emailCol.setCellValueFactory(new PropertyValueFactory<>("lastname"));
+        birthCol.setCellValueFactory(new PropertyValueFactory<>("birth"));
+        actionCol.setCellValueFactory(new PropertyValueFactory<>("menuButton"));
+        menuButton = new ArrayList<>();
+        data = FXCollections.observableArrayList();
+        for (int i = 0; i < listUser.get(0).size(); i++) {
+            menuButton.add(new MenuButton());
+            MenuItem edit = new MenuItem("edit");
+            MenuItem delete = new MenuItem("delete");
+            menuButton.get(i).setText("...");
+            menuButton.get(i).getItems().addAll(edit);
+            menuButton.get(i).getItems().addAll(delete);
+            int id = i;
+            edit.setOnAction(event -> {
+                editableCols();
+                System.out.println("edit");
+            });
+            delete.setOnAction(event -> {
+                System.out.println(id);
+                removeSelectedRows(Integer.parseInt(listUser.get(0).get(id)), tableView.getSelectionModel().getSelectedIndex());
+                System.out.println("remove row:" + tableView.getSelectionModel().getSelectedIndex());
+            });
+
+            data.addAll(new UserModel.User(listUser.get(0).get(i),
+                    listUser.get(1).get(i), listUser.get(2).get(i),listUser.get(3).get(i),
+                    listUser.get(4).get(i), listUser.get(5).get(i), listUser.get(6).get(i),
+                    listUser.get(7).get(i), (listUser.get(8).get(i)) , menuButton.get(i)) );
+
+
+        }
+        tableView.setItems(data);
+        tableView.setRowFactory(param -> new rowUser());
+        filter();
+    }
+
+    private void filter() {
+        filteredList = new FilteredList<>(tableView.getItems(), questionTableView -> true);
+        searchingField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredList.setPredicate(user ->  {
+                filterText.setVisible(false);
+                if (newValue == null || newValue.isEmpty())
+                    return true;
+                String lowerCaseFilter = newValue.toLowerCase();
+                if (user.getFirstname().contains(lowerCaseFilter))
+                    return true;
+                else return user.getLastname().contains(lowerCaseFilter);
+            });
+        });
+        filterText.setOnKeyPressed(event ->{
+            if (event.getEventType() != null)
+                filterText.setVisible(false);
+        } );
+
+        SortedList<UserModel.User> sortedList = new SortedList<>(filteredList);
+        sortedList.comparatorProperty().bind(tableView.comparatorProperty());
+        tableView.setItems(sortedList);
 
     }
 
+    private void removeSelectedRows(int parseInt, int selectedIndex) {
+        model.removeUser(parseInt);
+        data.remove(selectedIndex);
+    }
 
+    private void editableCols() {
 
+    }
+
+    public void addRow(){
+        menuButton.add(new MenuButton());
+        MenuItem edit = new MenuItem("edit");
+        MenuItem delete = new MenuItem("delete");
+        edit.setOnAction(event -> {
+            editableCols();
+            System.out.println("edit");
+        });
+        delete.setOnAction(event -> {
+            removeSelectedRows(Integer.parseInt((tableView.getSelectionModel().getSelectedItem().getUserInfoid())), tableView.getSelectionModel().getSelectedIndex());
+        });
+        menuButton.get(menuButton.size() -1).setText("...");
+        menuButton.get(menuButton.size() -1).getItems().addAll(edit);
+        menuButton.get(menuButton.size() -1).getItems().addAll(delete);
+        data.addAll(new UserModel.User(
+                idText.getText(),usernameText.getText(),passwordText.getText(),
+                addressText.getText(),emailText.getText(),phoneText.getText(),firstnameText.getText(),lastnameText.getText(),
+                birthText.getText(), menuButton.get(menuButton.size() -1))
+        );
+        model.addUser(new UserModel.User( idText.getText(),usernameText.getText(),passwordText.getText(),
+                addressText.getText(),emailText.getText(),phoneText.getText(),firstnameText.getText(),lastnameText.getText(),
+                birthText.getText(), menuButton.get(menuButton.size() -1)), false);
+
+        idText.clear();
+        usernameText.clear();
+        passwordText.clear();
+        firstnameText.clear();
+        lastnameText.clear();
+        addressText.clear();
+        phoneText.clear();
+        emailText.clear();
+        birthText.clear();
+    }
     @Override
     public void loadScene(String url, Object parent) {
         Parent user = null;
@@ -38,7 +207,47 @@ public class AdminUserController implements Initializable, LoadSceneAble {
 
     }
 
-    public void addRow(){
 
+
+    private static class rowUser extends TableRow<UserModel.User>{
+        private Label userInfoid;
+        private Label username;
+        private Label password;
+        private Label address ;
+        private Label email ;
+        private Label phone ;
+        private Label firstname ;
+        private Label lastname;
+        private Label birth ;
+        private MenuButton menuButton;
+        public rowUser(){
+            super();
+            userInfoid = new Label();
+            username = new Label();
+            password = new Label();
+            address = new Label();
+            email = new Label();
+            phone = new Label();
+            firstname = new Label();
+            lastname = new Label();
+            birth =  new Label();
+            menuButton = new MenuButton();
+        }
+
+        @Override
+        protected void updateItem(UserModel.User item, boolean empty) {
+            super.updateItem(item, empty);
+            if (item != null && !isEmpty()){
+                userInfoid.setText(item.getUserInfoid());
+                username.setText(item.getUsername());
+                password.setText(item.getPassword());
+                address.setText(item.getAddress());
+                email.setText(item.getEmail());
+                phone.setText(item.getPhone());
+                firstname.setText(item.getFirstname());
+                lastname.setText(item.getLastname());
+                birth.setText(item.getBirth());
+            }
+        }
     }
 }
--- src/model/UserModel.java
+++ src/model/UserModel.java
@@ -1,16 +1,218 @@
 package model;
 
-import javafx.fxml.Initializable;
+
+
+import helper.ConnectDataHelper;
+import javafx.scene.control.MenuButton;
 
 import java.net.URL;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
  * Created by traig on 10:18 AM, 11/15/2018
  */
-public class UserModel implements Initializable {
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+public class UserModel  {
 
+
+    public List<List<String>> getUserInfo(){
+        String query = "select * from userinfo";
+        List<List<String>> ans = new ArrayList<>();
+        try(Statement statement = ConnectDataHelper.getInstance().connectDB().createStatement()) {
+            statement.execute("use data");
+            ResultSet resultSet = statement.executeQuery(query);
+
+            List<String> userInfoid = new ArrayList<>();
+            List<String> username = new ArrayList<>();
+            List<String> password = new ArrayList<>();
+            List<String> address = new ArrayList<>();
+            List<String> email = new ArrayList<>();
+            List<String> phone = new ArrayList<>();
+            List<String> firstname = new ArrayList<>();
+            List<String> lastname = new ArrayList<>();
+            List<String> birth = new ArrayList<>();
+            while (resultSet.next()){
+                userInfoid.add(String.valueOf(resultSet.getInt("userInfoid")));
+                username.add(resultSet.getString("username"));
+                password.add(resultSet.getString("password"));
+                address.add(resultSet.getString("address"));
+                email.add(resultSet.getString("email"));
+                phone.add(String.valueOf(resultSet.getInt("phone")));
+                firstname.add(resultSet.getString("firstname"));
+                lastname.add(resultSet.getString("lastname"));
+                birth.add(String.valueOf(resultSet.getDate("birth")));
+            }
+            ans.add(userInfoid);
+            ans.add(username);
+            ans.add(password);
+            ans.add(address);
+            ans.add(email);
+            ans.add(phone);
+            ans.add(firstname);
+            ans.add(lastname);
+            ans.add(birth);
+        } catch (IllegalAccessException | InstantiationException | SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return ans;
     }
+
+    public boolean removeUser(int id){
+        String query = "delete from userInfo where userInfoid = " + id;
+        try(PreparedStatement statement = ConnectDataHelper.getInstance().connectDB().prepareStatement(query)) {
+            statement.execute("use data");
+            statement.executeUpdate();
+        } catch (IllegalAccessException | InstantiationException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean addUser(User user, boolean isSignup){
+        String query = "insert into userinfo values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        if (isSignup)
+            query = "insert into userinfo(username, password) values(?, ?)";
+        try(PreparedStatement statement = ConnectDataHelper.getInstance().connectDB().prepareStatement(query)) {
+            statement.execute("use data");
+            if (isSignup){
+                statement.setString(1, user.getUsername());
+                statement.setString(2, user.getPassword());
+            }else {
+                statement.setInt(1, Integer.parseInt(user.getUserInfoid()));
+                statement.setString(2,user.getUsername());
+                statement.setString(3, user.getPassword());
+                statement.setString(4, user.getAddress());
+                statement.setString(5,user.getEmail());
+                statement.setInt(6, Integer.parseInt(user.getPhone()));
+                statement.setString(7, user.getFirstname());
+                statement.setString(8, user.getLastname());
+                statement.setDate(9, Date.valueOf(user.getBirth()));
+            }
+
+            statement.executeUpdate();
+        } catch (IllegalAccessException | InstantiationException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+
+    public static class User{
+        private String userInfoid;
+        private String username;
+        private String password;
+        private String address = "Empty";
+        private String email = "Empty";
+        private String phone = "Empty";
+        private String firstname = "Empty";
+        private String lastname = "Empty";
+        private String birth = "1990-12-31";
+        private MenuButton menuButton;
+
+        public User(String userInfoid, String username, String password, String address, String email, String phone,
+                    String firstname, String lastname, String birth, MenuButton menuButton) {
+            this.userInfoid = userInfoid;
+            this.username = username;
+            this.password = password;
+            this.address = address;
+            this.email = email;
+            this.phone = phone;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.birth = birth;
+            this.menuButton = menuButton;
+        }
+
+        public User(String userInfoid, String username, String password, MenuButton menuButton) {
+            this.userInfoid = userInfoid;
+            this.username = username;
+            this.password = password;
+            this.menuButton = menuButton;
+        }
+
+        public String getUserInfoid() {
+            return userInfoid;
+        }
+
+        public void setUserInfoid(String userInfoid) {
+            this.userInfoid = userInfoid;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        public String getFirstname() {
+            return firstname;
+        }
+
+        public void setFirstname(String firstname) {
+            this.firstname = firstname;
+        }
+
+        public String getLastname() {
+            return lastname;
+        }
+
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+
+        public String getBirth() {
+            return birth;
+        }
+
+        public void setBirth(String birth) {
+            this.birth = birth;
+        }
+
+        public MenuButton getMenuButton() {
+            return menuButton;
+        }
+
+        public void setMenuButton(MenuButton menuButton) {
+            this.menuButton = menuButton;
+        }
+    }
+
 }
