d7867ddf14b93629f5b567abc1bcbe8399a70d81
taidev198 <traigiuxom9x@gmail.com>
2018-11-17 09:37:25 +0700

    demo 3:completing AdminQuizzTestView Scene.

--- src/controller/AdminQuizTestController.java
+++ src/controller/AdminQuizTestController.java
@@ -1,17 +1,242 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
+import model.QuizTestGrammarModel;
+import model.TestGrammarModel;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class AdminQuizTestController implements Initializable {
+
+    @FXML
+    TableView<QuizTestGrammarModel.QuizzQuesstionView> tableView;
+    @FXML
+    TableColumn<QuizTestGrammarModel.QuizzQuesstionView, String> idCol;
+    @FXML
+    TableColumn<QuizTestGrammarModel.QuizzQuesstionView, String> questionCol;
+    @FXML
+    TableColumn<QuizTestGrammarModel.QuizzQuesstionView, String> optionACol;
+    @FXML
+    TableColumn<QuizTestGrammarModel.QuizzQuesstionView, String> optionBCol;
+    @FXML
+    TableColumn<QuizTestGrammarModel.QuizzQuesstionView, String> optionCCol;
+    @FXML
+    TableColumn<QuizTestGrammarModel.QuizzQuesstionView, String> optionDCol;
+    @FXML
+    TableColumn<QuizTestGrammarModel.QuizzQuesstionView, String> keyCol;
+    @FXML
+    TableColumn<QuizTestGrammarModel.QuizzQuesstionView, String> contestidCol;
+    @FXML
+    TableColumn<QuizTestGrammarModel.QuizzQuesstionView, String> grammarIdCol;
+    @FXML
+    TableColumn<QuizTestGrammarModel.QuizzQuesstionView, MenuButton> actionCol;
+
+    ObservableList<QuizTestGrammarModel.QuizzQuesstionView> data;
+
+    @FXML
+    TextField idText;
+    @FXML
+    TextField questionText;
+    @FXML
+    TextField optionAText;
+    @FXML
+    TextField optionBText;
+    @FXML
+    TextField optionCText;
+    @FXML
+    TextField optionDText;
+    @FXML
+    TextField keyText;
+    @FXML
+    TextField contestidText;
+    @FXML
+    TextField grammarIdText;
+    @FXML
+    TextField searchingField;
+    @FXML
+    Text filterText;
+    List<MenuButton> menuButton;
+    QuizTestGrammarModel model;
+    FilteredList<QuizTestGrammarModel.QuizzQuesstionView> filteredList;
+    private  List<List<String>> listQuestion;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        model = new QuizTestGrammarModel();
+        idCol.setCellValueFactory(
+                new PropertyValueFactory<>("quizzcontestid"));
+        questionCol.setCellValueFactory(
+                new PropertyValueFactory<>("question"));
+        optionACol.setCellValueFactory(
+                new PropertyValueFactory<>("optionA"));
+        optionBCol.setCellValueFactory(
+                new PropertyValueFactory<>("optionB"));
+        optionCCol.setCellValueFactory(
+                new PropertyValueFactory<>("optionC"));
+        optionDCol.setCellValueFactory(
+                new PropertyValueFactory<>("optionD"));
+        keyCol.setCellValueFactory(
+                new PropertyValueFactory<>("key"));
+        contestidCol.setCellValueFactory(
+                new PropertyValueFactory<>("contestid"));
+        grammarIdCol.setCellValueFactory(
+                new PropertyValueFactory<>("grammarid"));
+
+        actionCol.setCellValueFactory(
+                new PropertyValueFactory<>("menuButton"));
+        listQuestion = model.getDB();
+        menuButton = new ArrayList<>();
+        data = FXCollections.observableArrayList();
+        for (int i = 0; i < listQuestion.get(0).size(); i++) {
+            menuButton.add(new MenuButton());
+            MenuItem edit = new MenuItem("edit");
+            MenuItem delete = new MenuItem("delete");
+            menuButton.get(i).setText("...");
+            menuButton.get(i).getItems().addAll(edit);
+            menuButton.get(i).getItems().addAll(delete);
+            int id = i;
+            edit.setOnAction(event -> {
+                editableCols();
+                System.out.println("edit");
+            });
+            delete.setOnAction(event -> {
+                System.out.println(id);
+                removeSelectedRows(Integer.parseInt(listQuestion.get(0).get(id)), tableView.getSelectionModel().getSelectedIndex());
+                System.out.println("remove row:" + tableView.getSelectionModel().getSelectedIndex());
+            });
+
+            data.addAll(new QuizTestGrammarModel.QuizzQuesstionView(listQuestion.get(0).get(i),
+                    listQuestion.get(1).get(i), listQuestion.get(2).get(i),listQuestion.get(3).get(i),
+                    listQuestion.get(4).get(i), listQuestion.get(5).get(i),
+                    listQuestion.get(6).get(i), (listQuestion.get(7).get(i)), (listQuestion.get(8).get(i)) , menuButton.get(i)) );
+
+
+        }
+        tableView.setItems(data);
+        tableView.setRowFactory(param -> new rowQuizzQuestion());
+        filter();
+
+    }
+
+    private void filter() {
+        filteredList = new FilteredList<>(tableView.getItems(), quizzQuesstionView  -> true);
+        searchingField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredList.setPredicate(quizzQuesstionView  -> {
+                filterText.setVisible(false);
+                if (newValue == null || newValue.isEmpty())
+                    return true;
+                String lowerCaseFilter = newValue.toLowerCase();
+                if (quizzQuesstionView.getQuestion().contains(lowerCaseFilter))
+                    return true;
+                else return quizzQuesstionView.getQuizzcontestid().contains(lowerCaseFilter);
+            });
+        });
+        filterText.setOnKeyPressed(event ->{
+            if (event.getEventType() != null)
+                filterText.setVisible(false);
+        } );
+
+        SortedList<QuizTestGrammarModel.QuizzQuesstionView> sortedList = new SortedList<>(filteredList);
+        sortedList.comparatorProperty().bind(tableView.comparatorProperty());
+        tableView.setItems(sortedList);
+    }
+
+    private void removeSelectedRows(int parseInt, int selectedIndex) {
+        model.removeQuizzTestGrammar(parseInt);
+        data.remove(selectedIndex);
+    }
+
+    private void editableCols() {
+
 
     }
 
     public void addRow(){
+        menuButton.add(new MenuButton());
+        MenuItem edit = new MenuItem("edit");
+        MenuItem delete = new MenuItem("delete");
+        edit.setOnAction(event -> {
+            editableCols();
+            System.out.println("edit");
+        });
+        delete.setOnAction(event -> {
+            removeSelectedRows(Integer.parseInt((tableView.getSelectionModel().getSelectedItem().getQuizzcontestid())), tableView.getSelectionModel().getSelectedIndex());
+        });
+        menuButton.get(menuButton.size() -1).setText("...");
+        menuButton.get(menuButton.size() -1).getItems().addAll(edit);
+        menuButton.get(menuButton.size() -1).getItems().addAll(delete);
+        data.addAll(new QuizTestGrammarModel.QuizzQuesstionView(
+                idText.getText(),questionText.getText(),optionAText.getText(),
+                optionBText.getText(),optionCText.getText(),optionDText.getText(),keyText.getText(),contestidText.getText(), grammarIdText.getText(),
+                menuButton.get(menuButton.size() -1))
+        );
+        model.addNewTestGrammar(new QuizTestGrammarModel.QuizzQuesstionView(
+                idText.getText(),questionText.getText(),optionAText.getText(),
+                optionBText.getText(),optionCText.getText(),optionDText.getText(),keyText.getText(),
+                contestidText.getText(), grammarIdText.getText(), menuButton.get(menuButton.size() -1))
+        );
 
+        questionText.clear();
+        optionAText.clear();
+        optionBText.clear();
+        optionCText.clear();
+        optionDText.clear();
+        grammarIdText.clear();
+        contestidText.clear();
+        idText.clear();
+        keyText.clear();
+
+    }
+
+    private static class rowQuizzQuestion extends TableRow<QuizTestGrammarModel.QuizzQuesstionView>{
+        private Label id;
+        private Label question;
+        private Label optionA;
+        private Label optionB;
+        private Label optionC;
+        private Label optionD;
+        private Label key;
+        private Label contestid;
+        private Label grammarid;
+        private MenuButton action;
+        public rowQuizzQuestion(){
+            super();
+            id = new Label();
+            question = new Label();
+            optionA = new Label();
+            optionB = new Label();
+            optionC = new Label();
+            optionD = new Label();
+            key = new Label();
+            grammarid = new Label();
+            contestid = new Label();
+        }
+
+        @Override
+        protected void updateItem(QuizTestGrammarModel.QuizzQuesstionView item, boolean empty) {
+            super.updateItem(item, empty);
+            if (item !=null && !empty){
+                id.setText(String.valueOf(item.getQuizzcontestid()));
+                question.setText(item.getQuestion());
+                optionA.setText(item.getOptionA());
+                optionB.setText(item.getOptionB());
+                optionC.setText(item.getOptionC());
+                optionD.setText(item.getOptionD());
+                key.setText(item.getKey());
+                grammarid.setText(item.getGrammarid());
+                contestid.setText(item.getContestid());
+            }else setGraphic(null);
+
+        }
     }
 }
--- src/model/QuizTestGrammarModel.java
+++ src/model/QuizTestGrammarModel.java
@@ -1,7 +1,209 @@
 package model;
 
+import helper.ConnectDataHelper;
+import javafx.scene.control.MenuButton;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by traig on 4:35 PM, 11/12/2018
  */
 public class QuizTestGrammarModel {
+
+    public QuizTestGrammarModel(){}
+
+    public List<List<String>> getDB(){
+        String query = " select * from quizzcontest";
+        List<List<String>> ans = new ArrayList<>();
+        try(Statement statement = ConnectDataHelper.getInstance().connectDB().createStatement()) {
+            statement.execute("use data");
+            ResultSet resultSet = statement.executeQuery(query);
+            List<String> quizzcontestid = new ArrayList<>();
+            List<String> question = new ArrayList<>();
+            List<String> a = new ArrayList<>();
+            List<String> b = new ArrayList<>();
+            List<String> c = new ArrayList<>();
+            List<String> d = new ArrayList<>();
+            List<String> key = new ArrayList<>();
+            List<String> contestid = new ArrayList<>();
+            List<String> grammarid = new ArrayList<>();
+            while (resultSet.next()){
+                quizzcontestid.add(resultSet.getString("quizzcontestid"));
+                question.add(resultSet.getString("question"));
+                a.add(resultSet.getString("a"));
+                b.add(resultSet.getString("b"));
+                c.add(resultSet.getString("c"));
+                d.add(resultSet.getString("d"));
+                key.add(resultSet.getString("key"));
+                contestid.add(String.valueOf(resultSet.getInt("contestid")));
+                grammarid.add(String.valueOf(resultSet.getInt("grammarid")));
+            }
+            ans.add(quizzcontestid);
+            ans.add(question);
+            ans.add(a);
+            ans.add(b);
+            ans.add(c);
+            ans.add(d);
+            ans.add(key);
+            ans.add(contestid);
+            ans.add(grammarid);
+        } catch (IllegalAccessException | InstantiationException | SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return ans;
+    }
+
+    public boolean removeQuizzTestGrammar(int id){
+        String query = "delete from quizzcontest where quizzcontestid = " + id;
+        try(PreparedStatement statement = ConnectDataHelper.getInstance().connectDB().prepareStatement(query)) {
+            statement.execute("use data");
+            statement.executeUpdate();
+        } catch (IllegalAccessException | InstantiationException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean addNewTestGrammar(QuizzQuesstion question){
+
+        String query = "insert into quizzcontest(quizzcontestid, question, a, b, c, d,quizzcontest.key,contestid, grammarid) values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        try(PreparedStatement statement = ConnectDataHelper.getInstance().connectDB().prepareStatement(query)) {
+            statement.execute("use data");
+            statement.setInt(1, Integer.parseInt(question.getQuizzcontestid()));
+            statement.setString(2, question.getQuestion());
+            statement.setString(3, question.getOptionA());
+            statement.setString(4, question.getOptionB());
+            statement.setString(5, question.getOptionC());
+            statement.setString(6, question.getOptionD());
+            statement.setString(7, question.getKey());
+            statement.setInt(8, Integer.parseInt(question.getContestid()));
+            statement.setInt(9, Integer.parseInt(question.getGrammarid()));
+            statement.executeUpdate();
+        } catch (IllegalAccessException | InstantiationException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+
+    public static class QuizzQuesstion{
+        private String quizzcontestid;
+        private String question;
+        private String optionA;
+        private String optionB;
+        private String optionC;
+        private String optionD;
+        private String key;
+        private String contestid;
+        private String grammarid;
+
+        public QuizzQuesstion(String quizzcontestid, String question, String optionA, String optionB, String optionC,
+                              String optionD, String key, String contestid, String grammarid) {
+            this.quizzcontestid = quizzcontestid;
+            this.question = question;
+            this.optionA = optionA;
+            this.optionB = optionB;
+            this.optionC = optionC;
+            this.optionD = optionD;
+            this.key = key;
+            this.contestid = contestid;
+            this.grammarid = grammarid;
+        }
+
+        public String getQuizzcontestid() {
+            return quizzcontestid;
+        }
+
+        public void setQuizzcontestid(String quizzcontestid) {
+            this.quizzcontestid = quizzcontestid;
+        }
+
+        public String getQuestion() {
+            return question;
+        }
+
+        public void setQuestion(String question) {
+            this.question = question;
+        }
+
+        public String getOptionA() {
+            return optionA;
+        }
+
+        public void setOptionA(String optionA) {
+            this.optionA = optionA;
+        }
+
+        public String getOptionB() {
+            return optionB;
+        }
+
+        public void setOptionB(String optionB) {
+            this.optionB = optionB;
+        }
+
+        public String getOptionC() {
+            return optionC;
+        }
+
+        public void setOptionC(String optionC) {
+            this.optionC = optionC;
+        }
+
+        public String getOptionD() {
+            return optionD;
+        }
+
+        public void setOptionD(String optionD) {
+            this.optionD = optionD;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public String getContestid() {
+            return contestid;
+        }
+
+        public void setContestid(String contestid) {
+            this.contestid = contestid;
+        }
+
+        public String getGrammarid() {
+            return grammarid;
+        }
+
+        public void setGrammarid(String grammarid) {
+            this.grammarid = grammarid;
+        }
+    }
+
+    public static class QuizzQuesstionView extends QuizzQuesstion{
+        private MenuButton menuButton;
+        public QuizzQuesstionView(String quizzcontestid, String question, String optionA, String optionB,
+                                  String optionC, String optionD, String key, String contestid, String grammarid, MenuButton menuButton) {
+            super(quizzcontestid, question, optionA, optionB, optionC, optionD, key, contestid, grammarid);
+            this.menuButton = menuButton;
+        }
+
+        public MenuButton getMenuButton() {
+            return menuButton;
+        }
+
+        public void setMenuButton(MenuButton menuButton) {
+            this.menuButton = menuButton;
+        }
+    }
 }
